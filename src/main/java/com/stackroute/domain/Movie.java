5a5a8f858246f40e194c268220f4430484cac939
Your Name <youremail@domain.com>
2019-09-25 18:53:49 +0530

    final commit

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,10 +1,16 @@
 package com.stackroute.domain;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Movie {
+public class Movie implements BeanNameAware,BeanFactoryAware,ApplicationContextAware {
 
     Actor actor;
 
@@ -21,5 +27,20 @@ public class Movie {
                     " ,Actor age ="+actor.getAge());
 
         }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("BeanFactory aware");
     }
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("Bean name is calling");
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Application context aware");
+    }
+}
 
