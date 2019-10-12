7970eaf391694b7287570a545178242db4f46bb6
harsha-mupparaju <54061163+harsha-mupparaju@users.noreply.github.com>
2019-10-12 12:04:49 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -13,10 +13,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-/*        XmlBeanFactory factory=new XmlBeanFactory(new ClassPathResource(AnnotationsConfig));
-        Movie movieInfo1=factory.getBean(Movie.class);
-        movieInfo1.displayMovieInfo();
-        */
+   
 
         ApplicationContext context=new AnnotationConfigApplicationContext(AnnotationsConfig.class);
         Movie movieInfo=context.getBean("movie",Movie.class);
